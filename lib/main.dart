f069620bc7be07ce96755b0881fa2ef7749c67d4
HarisLaeeq <harislaeeq2403@gmail.com>
2025-10-18 12:23:30 +0500

    add button

--- lib/main.dart
+++ lib/main.dart
@@ -1,35 +1,48 @@
 import 'package:flutter/material.dart';
 
 void main() {
-  runApp(MyPage());
+  runApp(SignIn());
 }
 
-class MyPage extends StatelessWidget {
+class SignIn extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Hello",
       theme: ThemeData(primarySwatch: Colors.indigo),
-      home: BList(),
+      home: SignInPage(),
     );
   }
 }
 
-class BList extends StatefulWidget {
+class SignInPage extends StatefulWidget {
   @override
-  State<BList> createState() {
-    return StudentsList();
+  State<SignInPage> createState() {
+    return SignInPageState();
   }
 }
 
-class StudentsList extends State<BList> {
+class SignInPageState extends State<SignInPage> {
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar(title: Text("Students List")),
-      body: Center(
-
+      body: Column(
+        children: [
+          ElevatedButton(
+            onPressed: () {
+              print("Circular Button Pressed!");
+            },
+            style: ElevatedButton.styleFrom(
+              shape: const CircleBorder(),   // 👈 Makes the button circular
+              padding: const EdgeInsets.all(24), // Controls the size of the circle
+              backgroundColor: Colors.blue,  // Button color
+              foregroundColor: Colors.white, // Icon/Text color
+              elevation: 6,
+            ),
+            child: const Icon(Icons.add, size: 28),
+          )
+        ],
       )
     );
   }
