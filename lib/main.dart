ef9a0f387afff313bddd3aa90955d13d1b6b17e0
HarisLaeeq <harislaeeq2403@gmail.com>
2025-10-18 15:46:45 +0500

    signin-signup-page

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,20 @@
 import 'package:flutter/material.dart';
+import 'signup_page.dart';
 
 void main() {
-  runApp(SignIn());
+  runApp(const MyApp());
 }
 
-class SignIn extends StatelessWidget {
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Hello",
-      theme: ThemeData(primarySwatch: Colors.indigo),
-      home: SignInPage(),
-    );
-  }
-}
-
-class SignInPage extends StatefulWidget {
-  @override
-  State<SignInPage> createState() {
-    return SignInPageState();
-  }
-}
-
-class SignInPageState extends State<SignInPage> {
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          ElevatedButton(
-            onPressed: () {
-              print("Circular Button Pressed!");
-            },
-            style: ElevatedButton.styleFrom(
-              shape: const CircleBorder(),   // 👈 Makes the button circular
-              padding: const EdgeInsets.all(24), // Controls the size of the circle
-              backgroundColor: Colors.blue,  // Button color
-              foregroundColor: Colors.white, // Icon/Text color
-              elevation: 6,
-            ),
-            child: const Icon(Icons.add, size: 28),
-          )
-        ],
-      )
+      title: "Flutter Auth App",
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: SignUpPage(),
+      debugShowCheckedModeBanner: false,
     );
   }
 }
