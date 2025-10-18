4a3c0fd252bd545730e389865cfa7f5510aa5a60
yousuf1925 <yousaf1925@gmail.com>
2025-10-18 16:21:26 +0500

    ui form

--- lib/main.dart
+++ lib/main.dart
@@ -9,46 +9,14 @@ void main() async {
 }
 
 class SignIn extends StatelessWidget {
+  const SignIn({super.key});
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
       title: "Hello",
       theme: ThemeData(primarySwatch: Colors.indigo),
       home: Wrapper(),
-    );
-  }
-}
-
-class SignInPage extends StatefulWidget {
-  @override
-  State<SignInPage> createState() {
-    return SignInPageState();
-  }
-}
-
-class SignInPageState extends State<SignInPage> {
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      body: Column(
-        children: [
-          ElevatedButton(
-            onPressed: () {
-              print("Circular Button Pressed!");
-            },
-            style: ElevatedButton.styleFrom(
-              shape: const CircleBorder(), // 👈 Makes the button circular
-              padding: const EdgeInsets.all(
-                24,
-              ), // Controls the size of the circle
-              backgroundColor: Colors.blue, // Button color
-              foregroundColor: Colors.white, // Icon/Text color
-              elevation: 6,
-            ),
-            child: const Icon(Icons.add, size: 28),
-          ),
-        ],
-      ),
     );
   }
 }
