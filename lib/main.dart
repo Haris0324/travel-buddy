b647ac756e1fdd823424162486ad15105cac3f01
HarisLaeeq <harislaeeq2403@gmail.com>
2025-10-19 13:12:46 +0500

    add rip

--- lib/main.dart
+++ lib/main.dart
@@ -1,22 +1,21 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
-import 'package:travel_buddy/wrapper.dart';
+import 'package:travel_buddy/add_trip.dart';
+import 'signup_page.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp();
-  runApp(SignIn());
+void main() {
+  runApp(const MyApp());
 }
 
-class SignIn extends StatelessWidget {
-  const SignIn({super.key});
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
 
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: "Hello",
+      title: "Flutter Auth App",
+      theme: ThemeData(primarySwatch: Colors.blue),
+      home: AddTripPage(),
       debugShowCheckedModeBanner: false,
-      home: Wrapper(),
     );
   }
 }
